5a609406a9baf0c62337231dca9f868e36925d09
tuningkiwi <tuningkiwi@gmail.com>
2023-12-27 16:23:42 +0900

    QT PRJ 용으로 OUTPUT PIN 5으로 변경

--- gpio_out/gpio_out.c
+++ gpio_out/gpio_out.c
@@ -1,20 +1,24 @@
 #include <stdio.h>
 #include <wiringPi.h>
 
-#define LED 13
+#define LED 5
 
 int main(){
 
 	//wiringPiSetup();//WIRING PI 모드 사용 
 	wiringPiSetupGpio();//BCM 모드 
 	pinMode(LED, OUTPUT);
-	int count =5;
-	while(count){
+	//int count =5;
+	while(1){
 		digitalWrite(LED, HIGH);
+		printf("ON\n");
+		fflush(stdout);
 		delay(1000);
 		digitalWrite(LED, LOW);
+		printf("OFF\n");
+		fflush(stdout);
 		delay(1000);
-		count--;
+		//count--;
 	}
 
 	//digitalWrite(LED,LOW);
