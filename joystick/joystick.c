93f6f3e2ba68a478cc0384d3e870e85f6ff1f672
tuningkiwi <tuningkiwi@gmail.com>
2023-12-27 16:22:10 +0900

    QT PRJ에서 JOYSTICK 프로그램에서 특정 데이터 추출을 위해 *SLICER 구분자로 사용

--- joystick/joystick.c
+++ joystick/joystick.c
@@ -5,7 +5,7 @@
 
 #define BASE 100
 #define SPI_CHAN 0 
-#define SW_PIN 5
+#define SW_PIN 7
 
 int main(){
 
@@ -15,6 +15,7 @@ int main(){
 
     mcp3004Setup(BASE, SPI_CHAN);
     printf("Rasoberrt PI SPI ADC Program\n");
+    fflush(stdout);
     int result =0;
     int val_x = 0; 
     int val_y = 0; 
@@ -22,15 +23,19 @@ int main(){
     while(1){
         if(digitalRead(SW_PIN)==0){
             printf("PUSH\t");
+            fflush(stdout);
             result = 0; 
         }else {
             printf("OFF\t");
+            fflush(stdout);
         }
 
         val_x = analogRead(BASE+0);
         printf("x: %4d\t",val_x);
+        fflush(stdout);
         val_y = analogRead(BASE+1);
         printf("y: %4d\t",val_y);
+        fflush(stdout);
 
         // >>> 
         if(val_x>=1020 && val_y >=850){
@@ -40,13 +45,14 @@ int main(){
             result--;
             delay(50);
         }
-        printf("result: %d\n",result);
+        printf("result: *%d*\n",result);
+        //printf("result:\t");
+        //fflush(stdout);
+        //printf("%d\n",result);
+        //fflush(stdout);
         delay(100);
 
     }
-
-
-
 
     return 0; 
 }
